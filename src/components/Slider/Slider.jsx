8416445584b3b455252328d4ef09f1324bf871ae
Nazmul Hasan Himel <himelspark96@gmail.com>
2023-04-05 04:57:06 +0600

    some change

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,18 +1,37 @@
 
 import React from 'react';
 import "./Slider.scss";
+import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import {sliderImages} from '../../utils/images';
+import { sliderImages } from '../../utils/images';
 
-const Slider = () => {
+const SliderImg = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  };
   return (
-    <div className = "hero-slider">
-      <div className='hero-slider-item'>
-        <img src = {sliderImages[1]} alt = "" />
-      </div>
+    <div className="hero-slider">
+      <Slider {...settings}>
+        <div className='hero-slider-item'>
+          <img src={sliderImages[0]} alt="" />
+        </div>
+        <div className='hero-slider-item'>
+          <img src={sliderImages[1]} alt="" />
+        </div>
+        <div className='hero-slider-item'>
+          <img src={sliderImages[2]} alt="" />
+        </div>
+      </Slider>
+
     </div>
   )
 }
 
-export default Slider
+export default SliderImg
